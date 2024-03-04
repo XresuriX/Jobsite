7130590f480f1be2aed369ec168b526e26dcd09f
Leslie Taffe <92220727+XresuriX@users.noreply.github.com>
2024-03-03 20:52:28 -0500

    test

--- dev/build.sh
+++ dev/build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-export DJANGO_SETTINGS_MODULE=src.settings.prod
+set DJANGO_SETTINGS_MODULE=src.settings.prod
 
 # Convert static asset files
 python manage.py collectstatic --no-input
